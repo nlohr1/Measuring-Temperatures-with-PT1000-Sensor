6719f0dbf0f036f69913f4361ca0a15c48649cf3
nlohr <49346586+nlohr1@users.noreply.github.com>
2021-01-25 21:13:19 +0100

    Update Adafruit_MAX31865_PT100-P1000-RTD-Sensor.ino

--- Adafruit_MAX31865_PT100-P1000-RTD-Sensor/Adafruit_MAX31865_PT100-P1000-RTD-Sensor.ino
+++ Adafruit_MAX31865_PT100-P1000-RTD-Sensor/Adafruit_MAX31865_PT100-P1000-RTD-Sensor.ino
@@ -22,7 +22,7 @@ Adafruit_MAX31865 thermo = Adafruit_MAX31865(10, 11, 12, 13);
 //Adafruit_MAX31865 thermo = Adafruit_MAX31865(10);
 
 // The value of the Rref resistor. Use 430.0 for PT100 and 4300.0 for PT1000
-#define RREF      4300.0
+#define RREF      4300.0  //to-do: measure this out exactly!
 // The 'nominal' 0-degrees-C resistance of the sensor
 // 100.0 for PT100, 1000.0 for PT1000
 #define RNOMINAL  1000.0
